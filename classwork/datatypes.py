40daf8436fb3084327dea86911a5e763edb4b140
anemhemanth <150318573+anemhemanth@users.noreply.github.com>
2025-07-01 12:07:07 +0000

    classwork

---
+++ classwork/datatypes.py
@@ -0,0 +1,31 @@
+a=10
+print(a) #integer
+print(type(a))
+b=10.0
+print(b) #float
+print(type(b))
+c="hemanth"
+print(c) #string
+print(type(c))
+d=[1,4,3,2]
+print(d)     
+print(type(d))   #list
+e=(1,2,3,12)
+print(e)
+print(type(e))  #tuple  
+f={'name':'hemanth','batch':14}
+print(f)
+print(type(f)) #dictionary
+g={1,2,4,6}
+print(g)
+print(type(g)) #set
+frozenset=[{"hi","hello"}]
+print(frozenset)
+print(type(frozenset)) #immutable set
+s=True
+print(s)
+print(type(s))  #boolean
+randomvar=None  #none type
+z=3+5j
+print(z)
+print(type(z)) #complex

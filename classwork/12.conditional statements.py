27671eff5e13304cef6b7e75e9a9924f1416f2a6
anemhemanth <150318573+anemhemanth@users.noreply.github.com>
2025-07-14 04:48:15 +0000

    classwork

---
+++ classwork/12.conditional statements.py
@@ -0,0 +1,36 @@
+#if and else
+items=['car','bike','scooty','watch']
+print("welcome to flipcart".center(80,'-'))
+searchitem=input("enter the item: ").lower()
+
+if searchitem in items:
+    print("found ")
+else:
+    print("not found")
+
+
+#weekend budget
+amount=int(input("Enter the amount you can spend for weekend:"))
+if amount>20000:
+    print("triop to goa")
+elif amount>15000:
+    print("Go for shopping")
+elif amount>10000:
+    print("Clubing")    
+elif amount>15000:
+    print("cafe")    
+elif amount>2000:
+    print("buffet")
+elif amount>500:
+    print("movie") 
+else:
+    print("save the money and sleep")  
+
+
+num=int(input("enter the number:"))
+if num>0:
+    print("positive number")
+elif num<0:
+    print("negative number")
+else:
+    print("zero")
